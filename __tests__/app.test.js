f0a821429d1e260747d75c45442e9f696b62bef6
mik-rzo <michael.panong3@gmail.com>
2023-05-12 15:34:32 +0100

    refactored GET /api/reviews/:review_id to include comment count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -94,21 +94,22 @@ describe('/api/reviews', () => {
     });
     describe('/:review_id', () => {
         describe('GET request', () => {
-            test('status 200 - respond with review object containing the following properties: review_id, title, review_body, designer, review_img_url, votes, category, owner, created_at', () => {
+            test('status 200 - respond with review object containing the following properties: review_id, title, review_body, designer, review_img_url, votes, category, owner, created_at, comment_count', () => {
                 return request(app)
-                    .get('/api/reviews/6')
+                    .get('/api/reviews/2')
                     .expect(200)
                     .then((response) => {
                         const { review } = response.body;
-                        expect(review.review_id).toBe(6);
-                        expect(review.title).toBe('Occaecat consequat officia in quis commodo.');
-                        expect(review.review_body).toBe('Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.');
-                        expect(review.designer).toBe('Ollie Tabooger');
-                        expect(review.review_img_url).toBe('https://images.pexels.com/photos/207924/pexels-photo-207924.jpeg?w=700&h=700');
-                        expect(review.votes).toBe(8);
-                        expect(review.category).toBe('social deduction');
-                        expect(review.owner).toBe('mallionaire');
-                        expect(review.created_at).toBe('2020-09-13T14:19:28.077Z');
+                        expect(review.review_id).toBe(2);
+                        expect(review.title).toBe('Jenga');
+                        expect(review.review_body).toBe('Fiddly fun for all the family');
+                        expect(review.designer).toBe('Leslie Scott');
+                        expect(review.review_img_url).toBe('https://images.pexels.com/photos/4473494/pexels-photo-4473494.jpeg?w=700&h=700');
+                        expect(review.votes).toBe(5);
+                        expect(review.category).toBe('dexterity');
+                        expect(review.owner).toBe('philippaclaire9');
+                        expect(review.created_at).toBe('2021-01-18T10:01:41.251Z');
+                        expect(review.comment_count).toBe(3);
                     });
             });
             test('status 404 - review id does not exist in the database', () => {
